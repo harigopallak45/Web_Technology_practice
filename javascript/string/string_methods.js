b38e2c24dd173e67e484f1284b7f050d7b3a56a4
harigopallak45 <harigopallak45@gmail.com>
2024-09-24 11:25:00 +0530

    update before css  completetation

---
+++ javascript/string/string_methods.js
@@ -0,0 +1,22 @@
+// var str='Bala'
+// var str1=str.padStart(20,'raj');
+// var str2=str.padEnd(20,'murugan');
+// console.log(str1);
+// console.log(str2);
+
+
+var str=`she sells seashells by the seashore,
+the shells she sells are surely seashells.
+so if she sells shells on the seashore,
+i am sure she sells seashore shells.`
+
+console.log(str.includes('on1')+` str.includes('on1')`);
+console.log(str.includes('are')+` str.includes('are')`);
+
+console.log(str.startsWith('are')+` str.startsWith('are')`);
+console.log(str.startsWith('she')+` str.startsWith('she')`);
+
+console.log(str.endsWith('she')+` str.endsWith('she')`);
+console.log(str.endsWith('shells')+` str.endsWith('shells')`);
++``
+
---
+++ javascript/string/stringmethods.js
@@ -0,0 +1,38 @@
+// var str="hariharan";
+// console.log(str.slice(1,6));
+// console.log(str.slice(-5,-1));
+
+// console.log(str.substring(2,4));
+// console.log(str.substring(3,5));
+
+// console.log(str.substr(2,2));
+// console.log(str.substr(3,2));
+
+
+// var str=`she sells seashells by the seashore,
+// the shells she sells are surely seashells.
+// so if she sells shells on the seashore,
+// i am sure she sells seashore shells.`
+
+// // console.log(str.indexOf("sells",4));
+// // console.log(str.lastIndexOf("sells"));
+
+
+// // console.log(str.replace("she","bhanu"));
+// // console.log(str.replaceAll("she","bhanu"));
+// // console.log(str.replace("she","bhanu"));
+// var s1=str.indexOf("sells",5);
+// var s1=str.slice(0,s1);
+// var s2=str.slice(s1+1);
+// var s2=s2.replace("sells","dontsells");
+// str=s1+s2;
+// console.log(str);
+
+
+// var s1="     HARI   "
+// console.log(s1.length);
+// console.log(s1);
+// console.log(s1.trim());
+// console.log(s1.trimStart());
+// console.log(s1.trimEnd());
+
